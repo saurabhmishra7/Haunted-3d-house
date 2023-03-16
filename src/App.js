e39162b0c35f0f7bb6575c5d3baeb03da0ff8b39
Saurabh Mishra <mishrasourabh8888@gmail.com>
2023-03-16 12:51:52 +0530

    Adding haunted house code

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import HauntedHouse from './pages/hauntedHouse/hauntedHouse';
+function App() {
+  return (
+    <div>
+      <HauntedHouse></HauntedHouse>
+    </div>
+  );
+}
+
+export default App;
