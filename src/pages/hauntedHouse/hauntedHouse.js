ca5913dac5a0096e9777e1368e193f7c397914c7
Saurabh Mishra <mishrasourabh8888@gmail.com>
2023-03-16 14:03:03 +0530

    Added changes

--- src/pages/hauntedHouse/hauntedHouse.js
+++ src/pages/hauntedHouse/hauntedHouse.js
@@ -27,13 +27,13 @@ export default function HauntedHouse() {
      */
     const textureLoader = new THREE.TextureLoader();
 
-    const doorColorTexture = textureLoader.load("Haunted-3d-house/textures/door/color.jpg");
-    const doorAlphaTexture = textureLoader.load("Haunted-3d-house/textures/door/alpha.jpg");
+    const doorColorTexture = textureLoader.load("/textures/door/color.jpg");
+    const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg");
     const doorAmbientOcclusionTexture = textureLoader.load(
       "/textures/door/ambientOcclusion.jpg"
     );
-    const doorHeightTexture = textureLoader.load("Haunted-3d-house/textures/door/height.jpg");
-    const doorNormalTexture = textureLoader.load("Haunted-3d-house/textures/door/normal.jpg");
+    const doorHeightTexture = textureLoader.load("/textures/door/height.jpg");
+    const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg");
     const doorMetalnessTexture = textureLoader.load(
       "/textures/door/metalness.jpg"
     );
@@ -41,7 +41,7 @@ export default function HauntedHouse() {
       "/textures/door/roughness.jpg"
     );
 
-    const bricksColorTexture = textureLoader.load("Haunted-3d-house/textures/bricks/color.jpg");
+    const bricksColorTexture = textureLoader.load("/textures/bricks/color.jpg");
     const bricksAmbientOcclusionTexture = textureLoader.load(
       "/textures/bricks/ambientOcclusion.jpg"
     );
@@ -52,11 +52,11 @@ export default function HauntedHouse() {
       "/textures/bricks/roughness.jpg"
     );
 
-    const grassColorTexture = textureLoader.load("Haunted-3d-house/textures/grass/color.jpg");
+    const grassColorTexture = textureLoader.load("/textures/grass/color.jpg");
     const grassAmbientOcclusionTexture = textureLoader.load(
       "/textures/grass/ambientOcclusion.jpg"
     );
-    const grassNormalTexture = textureLoader.load("Haunted-3d-house/textures/grass/normal.jpg");
+    const grassNormalTexture = textureLoader.load("/textures/grass/normal.jpg");
     const grassRoughnessTexture = textureLoader.load(
       "/textures/grass/roughness.jpg"
     );
